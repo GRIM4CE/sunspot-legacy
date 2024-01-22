80a178ac1e038d2133c80d89dd4bc3c812c7639c
Jon Leibham <jleibham@gmail.com>
2024-01-22 15:59:43 -0600

    fix: handle if no data is available in data.json when requesting

--- app/api/routes.py
+++ app/api/routes.py
@@ -5,7 +5,13 @@ api_bp = Blueprint('api', __name__)
 
 @api_bp.route('/')
 def index():
-    with open('./temp/data.json', 'r') as file:
-        data = json.load(file)
+    file_path = './temp/data.json'
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        with open(file_path, 'w') as file:
+            data = {'number': 0}
+            json.dump(data, file)
 
     return jsonify(data)
