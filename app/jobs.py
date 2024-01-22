edbb09bd39ce283c473e636e5d8ec108387ef86a
Jon Leibham <jleibham@gmail.com>
2024-01-22 16:07:41 -0600

    fix: handle if no data is available in data.json when requesting

--- app/jobs.py
+++ app/jobs.py
@@ -2,7 +2,6 @@ from .extensions import scheduler
 from .utils import incrementJSONFileNumber
 
 def counter():
-    print('hi')
     file_path = "./temp/data.json"
     incrementJSONFileNumber(file_path)
 
--- app/utils/fs.py
+++ app/utils/fs.py
@@ -10,9 +10,8 @@ def incrementJSONFileNumber(file_path):
         # Handle the case where the file doesn't exist or is not valid JSON
         current_number = 0
 
-    # Increase the number by 1
     new_number = current_number + 1
 
     # Write the updated number back to the JSON file
-    with open(file_path, 'w+') as file:
+    with open(file_path, 'w') as file:
         json.dump({'number': new_number}, file)
