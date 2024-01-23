62edefab65cacf42ca42d35483e3caa30ecefdee
Jon Leibham <jleibham@gmail.com>
2024-01-23 12:01:22 -0600

    feat: updates how count is set on data

--- app/db.py
+++ app/db.py
@@ -15,15 +15,27 @@ db = LocalProxy(get_db)
 
 def get_sunspot():
     try:
-        cursor = db.sunspot_records.find()
-        data_list = [doc for doc in cursor]
+        cursor = db.sunspot_records.find({}, { 'time': 1, 'count': 1, '_id': 0})
+        data_list = []
+        for doc in cursor:
+            # Format the datetime object to a string
+            formatted_time = doc['time'].strftime('%m/%d/%Y %H:%M')
+
+            # Create a new dictionary with the formatted time
+            formatted_doc = {
+                'time': formatted_time,
+                'count': doc['count']
+            }
+
+            data_list.append(formatted_doc)
+        
         return data_list
     except Exception as e:
         raise e 
     
 def create_sunspot_record():
     try:
-        current_datetime = datetime.now()
+        current_datetime = datetime.now().utcfromtimestamp()
         inserted_id = db.sunspot_records.insert_one({"time": current_datetime, "count": db.sunspot_records.count_documents({}) + 1}).inserted_id
         return inserted_id
     except Exception as e:
